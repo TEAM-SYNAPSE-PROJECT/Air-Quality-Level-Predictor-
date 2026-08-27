9c24533921ae587e5d80909d6a3bf226783235fa
Beulah <beulahjoyce97@gmail.com>
2026-08-27 20:52:37 +0530

    Fix Railway secrets variable names

--- create_secrets.py
+++ create_secrets.py
@@ -5,11 +5,11 @@ Path(".streamlit").mkdir(exist_ok=True)
 
 Path(".streamlit/secrets.toml").write_text(
     f'''[auth]
-redirect_uri = "{os.environ["GOOGLE_REDIRECT_URI"]}"
-cookie_secret = "{os.environ["GOOGLE_COOKIE_SECRET"]}"
-client_id = "{os.environ["GOOGLE_CLIENT_ID"]}"
-client_secret = "{os.environ["GOOGLE_CLIENT_SECRET"]}"
-server_metadata_url = "{os.environ["GOOGLE_SERVER_METADATA_URL"]}"
+redirect_uri = "{os.environ["redirect_uri"]}"
+cookie_secret = "{os.environ["cookie_secret"]}"
+client_id = "{os.environ["client_id"]}"
+client_secret = "{os.environ["client_secret"]}"
+server_metadata_url = "{os.environ["server_metadata_url"]}"
 ''',
     encoding="utf-8"
 )
